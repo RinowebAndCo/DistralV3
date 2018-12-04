bbe259fe98b0e796173ee84aaf65658d87d883ff
Rinoweb <marino.theo@gmail.com>
2018-12-04 16:05:43 +0100

    Initial commit

---
+++ README.txt
@@ -0,0 +1,1 @@
+DistralV2
---
+++ src/js/main.js
@@ -0,0 +1,34 @@
+﻿$(document).ready(function () {
+
+
+        "use strict";
+        Slider();
+        Sandwich();
+});
+
+function Slider() {
+    $('.slider').vegas({
+
+        slides: [{ src: 'images/slider/ariane.jpg' },
+        { src: 'images/slider/cloture.jpg' },
+        { src: 'images/slider/portail.jpg' },
+        { src: 'images/slider/premium.jpg' },
+        { src: 'images/slider/volet.jpg' }],
+        firstTransition: 'zoomOut',
+        firstTransitionDuration: '3000',
+        transition: 'fade',
+        animation: 'random',
+        timer: false,
+        shuffle: true,
+        preload: true,
+        overlay: 'images/slider/overlay.png',
+    }, $('.vegas-container').removeAttr('style'));
+}
+
+function Sandwich() {
+    $('.sandwich').click(function () {
+        $(this).toggleClass('change');
+        $('.menu').toggleClass('open');
+
+    });
+}
